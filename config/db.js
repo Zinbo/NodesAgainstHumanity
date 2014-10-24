14b88b86e18f754c90300ab041cf7d5243e4cee4
Shane <superzinbo@gmail.com>
2014-10-24 23:14:06 +0100

    set up proper db url

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
     module.exports = {
-        url : process.env.DATABASE_URL
+        url : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://Zinbo:pvg@proximus.modulusmongo.net:27017/Ju9wawaq'
     }
